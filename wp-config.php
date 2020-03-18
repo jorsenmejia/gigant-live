2609d81a174d4392f3cdc573ab5750cd5f5355e0
jorsen <jorsenmejia@gmail.com>
2020-03-18 16:23:01 +0800

    Update

--- wp-config.php
+++ wp-config.php
@@ -78,7 +78,7 @@ $table_prefix = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'WP_DEBUG', false );
-
+define( 'WP_MEMORY_LIMIT', '256M' );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
